3fe7a00f77efca84b551ff70791f2584eff9c403
Hana Klingová <hana.klingova@gmail.com>
2018-03-08 20:46:32 +0100

    implemented functions for dealing with cards status and add event click listener on all cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/**
- * declaring variable for the card deck
+/*
+ * declaring a variable for the whole deck div
  * declaring array holding the content of the cards
  */
 let deck = document.querySelector(".deck");
@@ -21,7 +21,7 @@ let cards = ["<i class=\"fas fa-gamepad\"></i>",
   "<i class=\"fas fa-university\"></i>"
 ];
 
-/**
+/*
  * shuffle function from http://stackoverflow.com/a/2450976
  */
 
@@ -40,15 +40,14 @@ function shuffle(array) {
   return array;
 }
 
-/**
+/*
  * function for dealing cards in shuffled order
- * create li items with class card
- * adds content to li items - the fontawesome icons
+ * create cards, add style to them and fontawesome icon
  */
 function dealCards(cards) {
   shuffle(cards);
   for (let i = 0; i < cards.length; i++) {
-    let cardItem = document.createElement("li");
+    let cardItem = document.createElement("div");
     cardItem.className = "card";
     let icon = cards[i];
     cardItem.innerHTML = icon;
@@ -56,4 +55,64 @@ function dealCards(cards) {
   }
 }
 
-dealCards(cards);
+/*
+ * function for removing cards from DOM
+ */
+function removeCards(cards) {
+  let cardsToRemove = document.querySelectorAll(".card");
+  for (let i = 0; i < cardsToRemove.length; i++) {
+    cardsToRemove[i].remove();
+  }
+}
+
+/*
+ *functions for changing the css of cards based on their status during the game
+ */
+function flipCard(card) {
+  card.classList.add("show");
+  flippedCards.push(card);
+}
+
+function matched(card) {
+  card.classList.add("matched");
+  flippedCards.pop(card);
+}
+
+function hideCard(card) {
+  card.classList.remove("show");
+  flippedCards.pop(card);
+}
+
+/*
+ * function for a new game
+ */
+function replay() {
+  removeCards();
+  dealCards(cards);
+}
+
+/*
+ * automatically deal cards when page is loaded
+ */
+document.addEventListener("DOMContentLoaded", dealCards(cards));
+
+/*
+ * handling click on a card
+ * 1) show card after click => call flipCard functions
+ * 2) check if list flippedCards has alredy 2 cards to be matched
+ * => if they match, use matched function on both cards
+ * => if they do not match, hide them (remove them from list and remove css class)
+ * => increment the counter for moves done by player and display it on the page
+ * 3) check if all cards have been matched
+ * 4) implement timer
+ * 5) handle the stars (desrease them according to number of moves)
+ * 6) display modal - with congrats, time, stars and replay option
+ */
+let allCards = document.querySelectorAll(".card");
+let flippedCards = [];
+
+allCards.forEach(function(card) {
+  card.addEventListener("click", function() {
+    console.log("clik on the card");
+  });
+});
