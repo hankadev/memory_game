f369232463d7a3583ed7c65b4b1c7b4875e6df1e
Hana Klingová <hana.klingova@gmail.com>
2018-03-08 14:40:52 +0100

    add javascript file with array of cards and notes for TODO

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/**
+ * declaring variable for the card deck
+ * declaring array holding the content of the cards
+ */
+let deck = document.querySelector(".deck");
+let cards = ["<i class=\"fas fa-gamepad\"></i>",
+  "<i class=\"fas fa-gamepad\"></i>",
+  "<i class=\"fas fa-ambulance\"></i>",
+  "<i class=\"fas fa-ambulance\"></i>",
+  "<i class=\"fas fa-birthday-cake\"></i>",
+  "<i class=\"fas fa-birthday-cake\"></i>",
+  "<i class=\"fas fa-chess\"></i>",
+  "<i class=\"fas fa-chess\"></i>",
+  "<i class=\"fab fa-fort-awesome\"></i>",
+  "<i class=\"fab fa-fort-awesome\"></i>",
+  "<i class=\"fas fa-bomb\"></i>",
+  "<i class=\"fas fa-bomb\"></i>",
+  "<i class=\"fas fa-plane\"></i>",
+  "<i class=\"fas fa-plane\"></i>",
+  "<i class=\"fas fa-university\"></i>",
+  "<i class=\"fas fa-university\"></i>"
+];
+
+/**
+ * shuffle function
+ */
+
+/** working piece of code for adding card to board
+ * need to be used on shuffled array and via loop
+  let cardItem = document.createElement("li");
+  cardItem.className = "card";
+  let icon = "<i class=\"fas fa-ambulance\"></i>"
+  cardItem.innerHTML = icon;
+  deck.appendChild(cardItem);
+*/
